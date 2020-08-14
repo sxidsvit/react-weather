d72f08b610b12614e2cecea4ce7eebc42ad2df77
sxidsvit <s.p.antonyuk@gmail.com>
2020-08-14 14:49:21 +0300

    hosting firebase

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,3 @@
 export const APIKEY = "72224e5b355d67283e0e9084d6bc395d"
-export const BASEURL = "http://api.openweathermap.org/data/2.5/weather"
+const PROXY = "https://cors-anywhere.herokuapp.com/"
+export const BASEURL = `${PROXY}http://api.openweathermap.org/data/2.5/weather`
