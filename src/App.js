e218d8401b98eb54492064fa81e0ac5c77072017
sxidsvit <s.p.antonyuk@gmail.com>
2020-08-14 03:31:59 +0300

    application uses the user’s location to fetch the current weather

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+
+  const [currentWeather, setSurrentWeather] = useState(null)
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition
+        (position => {
+          const { latitude: lat, longitude: lon } = position.coords
+          const apiKey = "72224e5b355d67283e0e9084d6bc395d"
+          const api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&APPID=${apiKey}&lang=ru`
+
+          fetch(api)
+            .then(response => response.json())
+            .then(data => {
+              setSurrentWeather(data)
+            })
+        })
+
+    } else {
+      alert("Geolocation is not supported by this browser")
+    }
+  }, [])
+
+  console.log('currentWeather: ', currentWeather)
+
   return (
     <div className="wrapper">
-      Hello
     </div>
   );
 }
