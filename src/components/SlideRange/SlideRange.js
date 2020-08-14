22abbe039aa57a01136c6ce68853b10767ef8602
sxidsvit <s.p.antonyuk@gmail.com>
2020-08-14 16:20:46 +0300

    refactoring SlideRange component

--- src/components/SlideRange/SlideRange.js
+++ src/components/SlideRange/SlideRange.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import './SlideRange.css'
+import { Form } from 'react-bootstrap'
 
 function SlideRange({ style, currentWeather, setSurrentWeather }) {
   const [temp, setTemp] = useState(0)
@@ -14,18 +15,18 @@ function SlideRange({ style, currentWeather, setSurrentWeather }) {
   }
 
   return (
-    <div className="slide-range" style={style}>
-      <label className="text-center d-block" htmlFor="customRange">Отрегулируйте температуру и цвет заднего фона</label>
-      <input type="range"
+    <Form className="slide-range" style={style}>
+      <Form.Label className="text-center d-block" htmlFor="customRange">Отрегулируйте температуру и цвет заднего фона</Form.Label>
+      <Form.Control type="range"
         className="custom-range"
-        min="-50"
-        max="50"
+        min="-30"
+        max="40"
         step="1"
         value={temp}
         id="customRange"
         onChange={changeTempHandler}
       />
-    </div>
+    </Form>
   )
 }
 
